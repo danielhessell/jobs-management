7827027a081b31b671949adfadf60cf7802ee06b
Daniel Hessel <danieldaniabreu@gmail.com>
2025-02-06 21:26:16 -0300

    chore: add exceptions handler

--- src/main/java/br/com/danielhessel/jobs_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/danielhessel/jobs_management/modules/candidate/controllers/CandidateController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.danielhessel.jobs_management.modules.candidate.CadidateEntity;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidates")
 public class CandidateController {
   
     @PostMapping("/")
-    public void create(@RequestBody CadidateEntity candidate) {
+    public void create(@Valid @RequestBody CadidateEntity candidate) {
         System.out.println("Candidato");
         System.out.println(candidate.getEmail());
     }
