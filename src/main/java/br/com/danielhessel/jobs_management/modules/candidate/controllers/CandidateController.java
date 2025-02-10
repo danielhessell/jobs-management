dd3bf3269903623ce0a8269ddd86bc9cb3f0fe3c
Daniel Hessel <danieldaniabreu@gmail.com>
2025-02-10 19:41:32 -0300

    feat: add repository and save candidate

--- src/main/java/br/com/danielhessel/jobs_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/danielhessel/jobs_management/modules/candidate/controllers/CandidateController.java
@@ -1,21 +1,25 @@
 package br.com.danielhessel.jobs_management.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.danielhessel.jobs_management.modules.candidate.CadidateEntity;
+import br.com.danielhessel.jobs_management.modules.candidate.CandidateEntity;
+import br.com.danielhessel.jobs_management.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidates")
 public class CandidateController {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
   
     @PostMapping("/")
-    public void create(@Valid @RequestBody CadidateEntity candidate) {
-        System.out.println("Candidato");
-        System.out.println(candidate.getEmail());
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidate) {
+        return this.candidateRepository.save(candidate);
     }
 
 }
