02d8a01862de32f09ac851eb649e75b37f7bda1c
Daniel Hessel <danieldaniabreu@gmail.com>
2025-02-11 15:12:34 -0300

    fix: add candidate hash password

--- src/main/java/br/com/danielhessel/jobs_management/modules/candidate/usecases/CreateCandidateUseCase.java
+++ src/main/java/br/com/danielhessel/jobs_management/modules/candidate/usecases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.danielhessel.jobs_management.modules.candidate.usecases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.danielhessel.jobs_management.exceptions.UserFoundException;
@@ -13,10 +14,16 @@ public class CreateCandidateUseCase {
     @Autowired
     CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidate) {
         this.candidateRepository.findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail()).ifPresent((cand) -> {
             throw new UserFoundException();
         });
+
+        var password = passwordEncoder.encode(candidate.getPassword());
+        candidate.setPassword(password);
 
         return this.candidateRepository.save(candidate);
     }
